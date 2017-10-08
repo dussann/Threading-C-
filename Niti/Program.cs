abf27effe1425fd22f1dbe7e7b1d2025706a64b2
Dusan Markovic <dussann1989@outlook.com>
2017-10-09 00:08:22 +0200

    Add project files.

---
+++ Niti/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Niti
+{
+    class Program
+    {
+        static object lockObject = new object();
+
+        public static void text()
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine("Thread runs");
+            }
+        }
+
+        public static void createThreadsSequential(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Thread t = new Thread(text);
+                t.Name = i.ToString();
+                t.Start();
+                Console.WriteLine("Thread " + t.Name + " is running: " + t.IsAlive);
+                t.Join();
+                Console.WriteLine("Thread " + t.Name + " is running: " + t.IsAlive);
+                Console.WriteLine();
+            }
+        }
+
+        public static void createThreads(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Thread t = new Thread(text);
+                t.Name = i.ToString();
+                t.Start();
+                Console.WriteLine("Thread " + t.Name + " is running: " + t.IsAlive);
+                //t.Join();
+                Console.WriteLine("Thread " + t.Name + " is running: " + t.IsAlive);
+                if (t.IsAlive)
+                {
+                    Console.WriteLine("ziv je");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Insert how many thrads do you want?");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            createThreadsSequential(number);
+            Console.ReadKey();
+        }
+    }
+}
